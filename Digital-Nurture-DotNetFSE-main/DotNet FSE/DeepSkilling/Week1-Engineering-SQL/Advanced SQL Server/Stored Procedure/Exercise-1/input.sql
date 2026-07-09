7cb20cbc508db0c9b9fba92c3275ae6802bfed20
Trisha Nandi <trishanandy285@gmail.com>
2026-07-10 00:54:31 +0530

    Add Week1-Engineering-SQL: Advanced SQL Server exercises including Ranking, Window Functions, and Stored Procedures

---
+++ Digital-Nurture-DotNetFSE-main/DotNet FSE/DeepSkilling/Week1-Engineering-SQL/Advanced SQL Server/Stored Procedure/Exercise-1/input.sql
@@ -0,0 +1,6 @@
+INSERT INTO Employees
+(FirstName, LastName, DepartmentID, Salary, JoinDate)
+VALUES
+('John','Smith',1,45000,'2023-01-10'),
+('Alice','Johnson',2,65000,'2022-05-15'),
+('Robert','Brown',2,70000,'2021-03-20');
